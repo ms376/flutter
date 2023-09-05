d35d6e421fea5d9ba1add9d8421f720945e07411
DESKTOP-25TV5SJ\minseok <rlaalstjr376@gmail.com>
2023-09-05 17:39:31 +0900

    no message

---
+++ ex07_button_deco/ex07_button_deco/android/app/src/main/java/com/study/ex07_button_deco/MainActivity.java
@@ -0,0 +1,6 @@
+package com.study.ex07_button_deco;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ex09_expanded/android/app/src/main/java/com/study/ex09_expanded/MainActivity.java
@@ -0,0 +1,6 @@
+package com.study.ex09_expanded;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
