30afee92088a1bc27317674d70104359603e95b4
DESKTOP-25TV5SJ\minseok <rlaalstjr376@gmail.com>
2023-09-19 17:37:28 +0900

    ~64

--- ex18_theme/lib/main.dart
+++ ex18_theme/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'color_schemes.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         unselectedWidgetColor: Colors.green,
---
+++ storage/lib/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:storage/imageupload/image_retrive.dart';
+import 'package:storage/imageupload/image_upload.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Storage Use"),
+      ),
+      body: Center(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ImageUpload(),
+                      ),
+                    );
+                  },
+                  child: const Text("Upload Image"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ImageRetrive(),
+                      ),
+                    );
+                  },
+                  child: const Text("Show Image"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
