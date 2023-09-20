be8eedd9ff7bbf057a9a7f4b6d4f0a2494a47d89
DESKTOP-25TV5SJ\minseok <rlaalstjr376@gmail.com>
2023-09-20 18:05:25 +0900

    netflix clone

--- nbbang/lib/main.dart
+++ nbbang/lib/main.dart
@@ -1,46 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'N / N',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'N-Bread'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+class HomeScreen extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 이미지 중앙 배치
+            Image.asset(
+              'assets/images/pt.sd.png', // 이미지 경로
+              width: 500.0,
+              height: 500.0,
+            ),
+            SizedBox(height: 20.0), // 간격 조절
+
+            // 사각형 버튼
+            ElevatedButton(
+              onPressed: () {
+                // 버튼 클릭 시 다른 페이지로 이동
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage()),
+                );
+              },
+              child: Text('시작하기'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class NextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('다음 페이지'),
       ),
-      body: const Center(
-        child: Column(
-          children: <Widget>[
-          ],
-        ),
+      body: Center(
+        child: Text('다음 페이지 내용'),
       ),
     );
   }
